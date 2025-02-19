98015a64a650051e5f55be756efeda3f8c562608
Evelyn6165 <phuonganhnguyen6165@gmail.com>
2025-02-20 01:54:39 +0200

    Finallize the project

--- wander_whimpsy.sql
+++ wander_whimpsy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 14, 2025 at 09:40 PM
+-- Generation Time: Feb 20, 2025 at 12:51 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -34,6 +34,35 @@ CREATE TABLE `blog_posts` (
   `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `blog_posts`
+--
+
+INSERT INTO `blog_posts` (`id`, `title`, `content`, `user_id`) VALUES
+(1, 'Helsinki - Finland', 'Finland was a great place to me. And Helsinki is a capital of Finland.', 5),
+(2, 'Porvoo Christmas 2024 - Worth the Visit?', 'Last year, we visited the Porvoo Christmas Market in early December. As we expected, the market was small. However, despite the frost, we were surprised by the variety of services and the warm atmosphere. 2024 Christmas in Porvoo will be a little different but share the same characteristics. Please read about last year\'s experiences to learn what will be different in 2024.\r\n\r\nThe Christmas market in Porvoo did not boast its size, but it was a great Christmas market for a small town. It was atmospherically built and centrally located. A free carousel and a homemade log fire to warm you up made it well worth a visit. You could also do some Christmas shopping at the market, and we recommend at least a cup of mulled wine with or without alcohol.\r\n\r\nPorvoo\'s 2024 Christmas Market, now called the Porvoo Christmas Path, is a delightful stroll through the city\'s charming streets. You can enjoy festive stalls, attractions, and events spread out along the path. Experience the magic of Christmas in Porvoo with beloved traditions and a joyous atmosphere. The path will be open from December 1 to 22, offering convenient hours for everyone to enjoy.', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `comment` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `post_id`, `user_id`, `comment`, `created_at`) VALUES
+(1, 1, 5, 'This is the great place', '2025-02-19 21:42:40');
+
 -- --------------------------------------------------------
 
 --
@@ -48,6 +77,13 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(5, 'anh123', 'anh123@mail.com', '$2y$10$2OdVTKMElLTAQSF72ynPE.NAle9ddSzETx.7zwQ/.fn7qpF/gPstq');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -56,6 +92,14 @@ CREATE TABLE `users` (
 --
 ALTER TABLE `blog_posts`
   ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `post_id` (`post_id`),
   ADD KEY `user_id` (`user_id`);
 
 --
@@ -72,13 +116,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `blog_posts`
 --
 ALTER TABLE `blog_posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
@@ -89,6 +139,13 @@ ALTER TABLE `users`
 --
 ALTER TABLE `blog_posts`
   ADD CONSTRAINT `blog_posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `blog_posts` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
